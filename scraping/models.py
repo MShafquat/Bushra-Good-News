cd508a45c75eb08a24689ae4a2be38c4660af39b
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-01-26 03:13:45 +0600

    Change image to image_url in news model class

--- scraping/models.py
+++ scraping/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     author = models.CharField(max_length=500)
     language = models.CharField(max_length=50)
     url = models.URLField(max_length=500)
-    image = models.ImageField(null=True)
+    image_url = models.URLField(max_length=500, null=True)
     body = models.TextField()
 
     def __str__(self):
