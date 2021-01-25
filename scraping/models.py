ee1d2ea9eb616b50b2e86dcfa10366fd3bc7df74
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-01-26 01:54:35 +0600

    Add news model

--- scraping/models.py
+++ scraping/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from djongo import models
+
 
 # Create your models here.
+class News(models.Model):
+    headline = models.CharField(max_length=250)
+    time = models.TimeField()
+    description = models.CharField(max_length=500, null=True)
+    image = models.ImageField(null=True)
+    author = models.CharField(max_length=500)
+    body = models.TextField()
+
