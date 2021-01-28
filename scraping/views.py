eb46eb7e3d297b8959ab16f2c48fb144dc0110b7
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-01-28 23:34:23 +0600

    Create api endpoint for scrape command

--- scraping/views.py
+++ scraping/views.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+
 from scraping.models import News
 from scraping.serializers import NewsSerializer
+from django.core.management import call_command
 
 
 class NewsViewSet(viewsets.ModelViewSet):
@@ -9,3 +14,8 @@ class NewsViewSet(viewsets.ModelViewSet):
     """
     queryset = News.objects.all().order_by('-pubdate')
     serializer_class = NewsSerializer
+
+    @action(detail=False, permission_classes=[IsAdminUser])
+    def scrape_news(self, request):
+        call_command('scrape')
+        return Response(status=status.HTTP_202_ACCEPTED)
