8ef39088dfdd22fa59761961be969009b219adec
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-01-28 20:44:04 +0600

    Create view for news api

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from scraping.models import News
+from scraping.serializers import NewsSerializer
 
-# Create your views here.
+
+class NewsViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allow news to be viewed
+    """
+    id_list = [news.id for news in News.objects.all().order_by('-pubdate') if news.is_positive()]
+    queryset = News.objects.filter(id__in=id_list)
+    serializer_class = NewsSerializer
