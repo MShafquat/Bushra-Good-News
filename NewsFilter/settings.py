f3f9606f409570f51b1e39f99236e91d669b94e5
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2021-01-29 21:53:29 +0600

    Add cors header config

--- NewsFilter/settings.py
+++ NewsFilter/settings.py
@@ -44,9 +44,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'NewsFilter.urls'
 
 TEMPLATES = [
